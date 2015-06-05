ba8333d77505192fa8fea6108960b288295969f0
Stuart Watt <stuart@morungos.com>
2015-06-05 14:27:44 -0400

    Added tests for multiple events

--- src/test/java/com/morungos/xmlhttprequest/InjectionTest.java
+++ src/test/java/com/morungos/xmlhttprequest/InjectionTest.java
@@ -2,7 +2,9 @@ package com.morungos.xmlhttprequest;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,7 +16,11 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
+import jdk.nashorn.api.scripting.JSObject;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -25,16 +31,15 @@ public class InjectionTest extends ScriptingTestBase {
 	public ExpectedException thrown = ExpectedException.none();
 	
 	private Map<String, Object> applicationProperties = new HashMap<String, Object>();
-
-	@Test
-	public void testInjectEvent() throws ScriptException, InterruptedException {
-		
-		applicationProperties.clear();
-		applicationProperties.put("interrupted", new Boolean(false));
-		
-		final ScriptEngine engine = getEngine();
+	
+	@Before
+	public void setUp() {
+		applicationProperties.clear();		
+	}
+	
+	private Thread initializeEngineThread(ScriptEngine engine) throws ScriptException {
 		engine.put("applicationProperties", applicationProperties);
-		
+
 		InputStream resourceStream = getClass().getClassLoader().getResourceAsStream("polyfill.nashorn.js");
 		
 		engine.eval(new InputStreamReader(resourceStream));
@@ -43,17 +48,20 @@ public class InjectionTest extends ScriptingTestBase {
 		engine.eval(wrapped);
 
 		Invocable inv = (Invocable) engine;
-				
 		Runnable r = inv.getInterface(Runnable.class);
-		
 		Thread th = new Thread(r);
+		
+		return th;
+	}
+
+	@Test
+	public void testInjectOneEvent() throws ScriptException, InterruptedException {
+		
+		applicationProperties.put("interrupted", new Boolean(false));
+		final ScriptEngine engine = getEngine();
+		Thread th = initializeEngineThread(engine);
 		th.start();
 		
-		// Now. At this stage we can theoretically inject new function calls. 
-		// Theoretically.... 
-		// We can do this by pushing them directly in as a new timer task. 
-
-		Phaser phaser = (Phaser) applicationProperties.get("phaser");
 		Timer timer = (Timer) applicationProperties.get("timer");
 		
 		Thread.sleep(200);
@@ -72,4 +80,43 @@ public class InjectionTest extends ScriptingTestBase {
 		Assert.assertTrue((Boolean)applicationProperties.get("interrupted"));
 	}
 
+	@Test
+	public void testInjectMultipleEvents() throws ScriptException, InterruptedException {
+		applicationProperties.put("events", new ArrayList<Object>());
+		final ScriptEngine engine = getEngine();
+		Thread th = initializeEngineThread(engine);
+		th.start();
+
+		Timer timer = (Timer) applicationProperties.get("timer");
+
+		String wrapped = "function trigger(event) { applicationProperties.events.add(event.event); }";
+		engine.eval(wrapped);
+
+		for(int i = 0; i < 3; i++) {
+			Thread.sleep(200);
+			
+			final int j = i;
+			
+			timer.schedule(new TimerTask() {
+				public void run() {
+					try {
+						engine.eval("setTimeout(function() { trigger({event: " + j + "}); if (" + j + " == 2) { shutdown(); }; }, 0);");
+					} catch (ScriptException e) {
+						e.printStackTrace();
+					}
+				}
+			}, 0);
+		}
+
+		th.join();
+		
+		@SuppressWarnings("unchecked")
+		List<Object> events = (List<Object>) applicationProperties.get("events");
+		
+		Assert.assertEquals(3, events.size());
+		for(int i = 0; i < 3; i++) {
+			Integer event = (Integer) events.get(i);
+			Assert.assertEquals(new Integer(i), event);
+		}
+	}
 }
